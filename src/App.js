53ec34946402e7c47eb5b213b0accbfbd88ea365
Mauro <mauromaio.dev@gmail.com>
2023-09-25 17:06:21 -0300

    feat: remove robots and new generateRandomColor

--- src/App.js
+++ src/App.js
@@ -70,14 +70,14 @@ function App() {
     localStorage.removeItem("gameHistory");
   };
 
-  const generateRandomColor = useCallback(() => {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }, []);
+  const generateRandomColor = useCallback(
+    () =>
+      `#${((Math.random() * 0xffffff) | 0)
+        .toString(16)
+        .padStart(6, "0")
+        .toUpperCase()}`,
+    []
+  );
 
   const initializeCurrentColor = useCallback(() => {
     const newColor = generateRandomColor();
