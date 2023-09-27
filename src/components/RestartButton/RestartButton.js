da1da4b2d51d50a982e68a9ca8ae35fc00cf8762
Mauro <mauromaio.dev@gmail.com>
2023-09-27 13:33:04 -0300

    feat: restart button testid

--- src/components/RestartButton/RestartButton.js
+++ src/components/RestartButton/RestartButton.js
@@ -3,7 +3,7 @@ import "./RestartButton.css";
 
 function RestartButton({ onClick, disabled }) {
   return (
-    <div className="restart-button-container">
+    <div className="restart-button-container" data-testid="restart-button">
       <button onClick={onClick} className="restart-button" disabled={disabled}>
         RESTART
       </button>
