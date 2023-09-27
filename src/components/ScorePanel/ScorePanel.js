3ce2402778bb0b77d1f1c066c5c034b8e27e8a39
Mauro <mauromaio.dev@gmail.com>
2023-09-27 13:32:48 -0300

    feat: score panel testid

--- src/components/ScorePanel/ScorePanel.js
+++ src/components/ScorePanel/ScorePanel.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ScorePanel({ highScore, currentScore }) {
   return (
-    <div className="score-panel">
+    <div className="score-panel" data-testid="score-panel">
       <p>High Score: {highScore}</p>
       <p>Score: {currentScore}</p>
     </div>
