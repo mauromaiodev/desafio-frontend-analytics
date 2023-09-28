0b58cb71528841ce11fde38e4f5ccb2ba6c147f4
Mauro <mauromaio.dev@gmail.com>
2023-09-28 12:55:06 -0300

    feat: sidebar testid

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./SideBar.css";
 
 function Sidebar({ children }) {
-  return <aside className="sidebar">{children}</aside>;
+  return (
+    <aside className="sidebar" data-testid="sidebar">
+      {children}
+    </aside>
+  );
 }
 
 export default Sidebar;
