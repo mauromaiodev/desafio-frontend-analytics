4ca9fd0aae0dd6f5ddff683a0872bec3c0631901
Mauro <mauromaio.dev@gmail.com>
2023-09-28 12:54:42 -0300

    feat: startpanel data id

--- src/components/StartPanel/StartPanel.js
+++ src/components/StartPanel/StartPanel.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function StartPanel({ startGame }) {
   return (
-    <div className="start-panel">
+    <div className="start-panel" data-testid="start-panel">
       <button onClick={startGame}>START</button>
     </div>
   );
